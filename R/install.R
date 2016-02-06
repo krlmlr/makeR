304a4f886ce481e0e485fe3299cbbbd23b4c8a9d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-02-06 01:43:22 +0100

    tweak result

--- R/install.R
+++ R/install.R
@@ -3,7 +3,10 @@ install_hooks <- function(pkg = ".") {
   pkg <- devtools::as.package(pkg)
 
   hooks_path <- system.file("gitflow", package = PACKAGE_NAME)
+  hooks_files <- dir(hooks_path)
+  hooks_abs_files <- file.path(hooks_path, hooks_files)
+
   target_path <- file.path(pkg$path, ".git", "hooks")
 
-  file.symlink(dir(hooks_path, full.names = TRUE), target_path)
+  invisible(setNames(file.symlink(hooks_abs_files, target_path), hooks_files))
 }
