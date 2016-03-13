7c0a6504946c7bee9fe2b8fa62a209cb57ec1e87
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-13 12:32:37 +0100

    new install_makefile()

--- R/install.R
+++ R/install.R
@@ -10,3 +10,23 @@ install_hooks <- function(pkg = ".") {
 
   invisible(setNames(file.symlink(hooks_abs_files, target_path), hooks_files))
 }
+
+#' @export
+install_makefile <- function(pkg = ".", filename = "Makefile",
+                             overwrite = FALSE) {
+  pkg <- devtools::as.package(pkg)
+
+  text <- c(
+    "MAKEFILE_LOC := $(shell Rscript -e \"cat(system.file('Makefile',package='" %+%
+      .packageName %+% "'))\" )",
+    "include ${MAKEFILE_LOC}"
+  )
+
+  file_path <- file.path(pkg$path, filename)
+  if (!overwrite && file.exists(file_path)) {
+    stop("Package ", pkg$package, " already has file ", filename,
+         call. = FALSE)
+  }
+
+  writeLines(text, file_path)
+}
